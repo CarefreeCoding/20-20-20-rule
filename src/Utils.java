699cc2791d226ac3ae58614803c4d0fcced12f3b
CarefreeCoding <carefree.coding@gmail.com>
2014-10-24 17:08:14 -0400

    Organizing messages
    A simple utilities class that just for now has a method for organizing
    the order in which the messages will be display.

---
+++ src/Utils.java
@@ -0,0 +1,95 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Utils
+{
+	public static LinkedList<Message> organizeMessages(
+			List<MessageInput> messageInputs)
+	{
+		// find smallest and largest frequency in the messages
+		int max = 0;
+		int min = 9999;
+		int frequency;
+		for (MessageInput input : messageInputs)
+		{
+			if (!input.isEnabled())
+			{
+				continue;
+			}
+			frequency = input.getFrequency();
+			if (max < frequency)
+			{
+				max = frequency;
+			}
+			if (min > frequency)
+			{
+				min = frequency;
+			}
+		}
+
+		// find common divisible number
+		int maxLoop;
+		boolean found;
+		for (int i = 1; ; i++)
+		{
+			maxLoop = max * i;
+			found = true;
+			for (MessageInput input : messageInputs)
+			{
+				if (!input.isEnabled())
+				{
+					continue;
+				}
+				if (maxLoop % input.getFrequency() != 0)
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+			{
+				break;
+			}
+		}
+
+		// find common step size
+		int stepSize = 1;
+		for (int i = max; i > 1; i--)
+		{
+			found = true;
+			for (MessageInput input : messageInputs)
+			{
+				if (!input.isEnabled())
+				{
+					continue;
+				}
+				if (input.getFrequency() % i != 0)
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+			{
+				stepSize = i;
+			}
+		}
+
+		LinkedList<Message> messages = new LinkedList<Message>();
+		for (int i = 1; i <= maxLoop; i += stepSize)
+		{
+			for (MessageInput input : messageInputs)
+			{
+				if (!input.isEnabled())
+				{
+					continue;
+				}
+				if (1 % input.getFrequency() == 0)
+				{
+					messages.add(input.getMessage());
+				}
+			}
+		}
+		return messages;
+	}
+}
