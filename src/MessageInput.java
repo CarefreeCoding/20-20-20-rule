5d0e89ee4b9dd50ad33a52922328bb2ff45e0185
CarefreeCoding <carefree.coding@gmail.com>
2014-10-26 17:04:56 -0400

    Error in Message input
    Message input should have two different models, one for frequency and
    one for duration.

--- src/MessageInput.java
+++ src/MessageInput.java
@@ -13,12 +13,13 @@ public class MessageInput extends JPanel
 	{
 		setLayout(new MigLayout("wrap 10, gapx 7"));
 
-		SpinnerNumberModel spinnerModel = new SpinnerNumberModel(5, 3, 35, 1);
+		SpinnerNumberModel frequencyModel = new SpinnerNumberModel(5, 3, 35, 1);
+		SpinnerNumberModel durationModel = new SpinnerNumberModel(5, 3, 35, 1);
 
 		enabled = new JCheckBox();
 		message = new JTextField("Your text here");
-		frequencySpinner = new JSpinner(spinnerModel);
-		durationSpinner = new JSpinner(spinnerModel);
+		frequencySpinner = new JSpinner(frequencyModel);
+		durationSpinner = new JSpinner(durationModel);
 
 		add(enabled);
 		add(new JLabel("Show text "));
