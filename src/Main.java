bb0eb2c871036f4ee5d3b6bcf8cd25c678f3bc0f
CarefreeCoding <carefree.coding@gmail.com>
2014-10-26 17:50:12 -0400

    And we have a main
    Now we have a main.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ControlWindow window = new ControlWindow();
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
